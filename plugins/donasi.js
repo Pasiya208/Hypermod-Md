ee7a0dab07a63d3e49ce67f9050d13c14b6833f3
Lakshitha <69718607+HYPER-MOD@users.noreply.github.com>
2021-12-30 16:06:48 +0000

    hypermod-md by lakshitha

---
+++ plugins/donasi.js
@@ -0,0 +1,55 @@
+/**
+ * TOLONG JANGAN GANTI GAMBARNYA,NOMORKU DAN SAWERIAKU
+ * MENDING KALIAN TAMBAHIN NOMOR KALIAN
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let handler = async (m) => {
+let duit = `*────── 「 HYPER MOD 」 ──────*
+
+
+Hi
+https://youtube.com/c/HYPERMOD`
+let message = await prepareWAMessageMedia({ image: {url: 'https://i.ibb.co/FwMVtQd/20211204-120943.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '🏧 MY WEBSITE',
+               url: 'www.hypermodz.ml'
+             }
+
+           },
+               {
+             callButton: {
+               displayText: 'Call',
+               phoneNumber: '+94 76-704-3432'
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
+
+module.exports = handler
