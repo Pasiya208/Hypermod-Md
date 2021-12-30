7bb219740dc955e2c030c552079b22d347a71628
Lakshitha <69718607+HYPER-MOD@users.noreply.github.com>
2021-12-30 16:47:03 +0000

    Add suit game

---
+++ plugins/suit.js
@@ -0,0 +1,46 @@
+let handler = async (m, { text, usedPrefix }) => {
+    let salah = `පවතින විකල්ප\n\nකතුර, කඩදාසි, ගල\n\n${usedPrefix}suit කතුර\n\nඅවකාශයට ආදරය කරන්න!`
+    if (!text) throw salah
+    var astro = Math.random()
+
+    if (astro < 0.34) {
+        astro = 'ගල'
+    } else if (astro > 0.34 && astro < 0.67) {
+        astro = 'කතුර'
+    } else {
+        astro = 'කඩදාසි'
+    }
+
+    //menentukan rules
+    if (text == astro) {
+        m.reply(`Seri!\nඔබ: ${text}\nBot: ${astro}`)
+    } else if (text == 'ගල') {
+        if (astro == 'කතුර') {
+            global.db.data.users[m.sender].money += 1000
+            m.reply(`ඔබ දිනුම්! +Rp1000\nඔබ: ${text}\nබොට්: ${astro}`)
+        } else {
+            m.reply(`ඔබ පැරදුණා!\nඔබ: ${text}\nBot: ${astro}`)
+        }
+    } else if (text == 'කතුර') {
+        if (astro == 'කඩදාසි') {
+            global.db.data.users[m.sender].money += 1000
+            m.reply(`ඔබ දිනුම්! +Rp1000\nඔබ: ${text}\nබොට්: ${astro}`)
+        } else {
+            m.reply(`ඔබ පරාදයි!\nඔබ: ${text}\nබොට්: ${astro}`)
+        }
+    } else if (text == 'කඩදාසි') {
+        if (astro == 'ගල') {
+            global.db.data.users[m.sender].money += 1000
+            m.reply(`ඔබ දිනුම්! +Rp1000\nඔබට: ${text}\nබොට්: ${astro}`)
+        } else {
+            m.reply(`ඔබ පරාදයි!\nඔයා: ${text}\nබොට්: ${astro}`)
+        }
+    } else {
+        throw salah
+    }
+}
+handler.help = ['suit']
+handler.tags = ['game']
+handler.command = /^(suit)$/i
+
+module.exports = handler
