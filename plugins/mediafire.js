203fe0380f2ce81a1a886caf896412a4e177eda0
Lakshitha <69718607+HYPER-MOD@users.noreply.github.com>
2021-12-31 19:52:20 +0530

    fix mediafıre name

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -26,7 +26,7 @@ m.reply(result)
 
 //await sleep(100)
 
-conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
+conn.sendFile(m.chat, res[0].link, ${res[0].nama}, null, m, false, {asDocument:true, mimetype:res[0].mime})
 
 }
 
